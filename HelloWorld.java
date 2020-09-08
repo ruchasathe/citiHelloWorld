b415d072e31144893c3325367aec8c8c9423f8c8
rucha sathe <imrucha98@gmail.com>
2020-09-08 11:34:22 +0000

    Hello World 5 times

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String args[]){
-		for(i = 0; i < 6; i++)
+		for(int i = 0; i < 6; i++)
 			System.out.println("Hello World, from NEW feature branch");
 	}
 }
